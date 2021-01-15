df84566d38f3d2ff0c67e7d32887809525ebab14
Yuriy <xaosmmm@gmail.com>
2021-01-15 21:57:53 +0200

    Added Index page

---
+++ ShopSite(Asp_Core_MVC)/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopSite_Asp_Core_MVC_.Data.Interfaces;
+using ShopSite_Asp_Core_MVC_.Models;
+
+namespace ShopSite_Asp_Core_MVC_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRepository;
+        private readonly ShopCart _shopCart;
+
+        public HomeController(IAllCars carRepository, ShopCart shopCart)
+        {
+            _carRepository = carRepository;
+            _shopCart = shopCart;
+
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel();
+
+            homeCars.favCars = _carRepository.GetFavoriteCars;
+            
+            return View(homeCars);
+        }
+
+        
+    }
+}
---
+++ ShopSite(Asp_Core_MVC)/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopSite_Asp_Core_MVC_.Data.Models;
+
+namespace ShopSite_Asp_Core_MVC_.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> favCars { get; set; }
+    }
+}
